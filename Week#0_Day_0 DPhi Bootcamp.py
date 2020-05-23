0f19c12846f117405bb4f19b301072e36bebc1c8
Harshit Singh <harshit9665@gmail.com>
2020-05-23 13:22:33 +0530

    Add files via upload

---
+++ Week#0_Day_0 DPhi Bootcamp.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# A shortcut for adding comments is by using CTRL + /
+
+
+# In[2]:
+
+
+a="hello world"
+
+
+# In[3]:
+
+
+print(a[4:8])
+
+
+# In[7]:
+
+
+# 1.Write a program to take a number from the user using a prompt “Enter a number : “.
+x=input("Enter a number :")
+
+
+# In[8]:
+
+
+# 2.Print the number taken by the user in the previous question.
+print(x)
+
+
+# In[10]:
+
+
+# 3.Print the square of that number.
+a=5**2
+print(a)
+
+
+# In[11]:
+
+
+# 4.Create a string with value “Program” and print ‘o’ from it. 
+b="Program"
+print(b[2])
+
+
+# In[12]:
+
+
+# 5.Print the last 4 characters of the above string.
+print(b[3:7])
+
+
+# In[14]:
+
+
+# 6.Check if the string contains all lowercase letters.
+b.islower()
+
+
+# In[15]:
+
+
+# 7.Print the type of the string
+type(b)
+
+
+# In[19]:
+
+
+# 8.Take in first name and last name as input and store it in the variables firstname and lastname
+# Suppose
+# firstname = “abc”
+# lastname = “xyz”,
+# Your task is to print the following:
+# # Hello abc xyz! You just delved into python.
+firstname=input("Enter firstname :")
+lastname=input("Enter Last Name :")
+name=firstname+" "+lastname
+print("Hello" +" " + name + "!You just delved into python. ")
+
+
+# In[ ]:
+
+
+
+
